751e03a970640611033bd5118d5cf73b282af001
matriuskaAmanda <matriuskamanda@gmail.com>
2024-05-11 20:57:21 -0600

    terminada guia 6

--- Guia de Ejercicios 6/exercise_5.js
+++ Guia de Ejercicios 6/exercise_5.js
@@ -79,26 +79,76 @@ const compras = {
         } 
     }       
 }
-//monto maximo registrado en una compra por cada proveddor y el numero de provedor
+
+const MES_AGOSTO = 8;
 let montoMaximo = 0;
 let montoCompra = 0;
 let kyeActual = 0;
+let tipoFacA = 0;
+let tipoFacB = 0;
+let tipoFacC = 0;
+let menorCompraAgosto = null;
+let nuProductoAgosto = 0;
+let maxCantidadComp = 0;
+let nuProductomaxCompra = 0;
+let provedorMayorcantidadComprada = 0;
+
 for(const key of Object.keys(compras)){
-    montoCompra = compras[key].cantidad * compras[key].precioUnitario
-    kyeActual = key
-    if(montoCompra > montoMaximo){
-        montoMaximo = montoCompra
-        console.log(montoMaximo, kyeActual)
+    for(const subkey of Object.keys(compras[key])){
+        montoCompra = compras[key][subkey].precioUnitario * compras[key][subkey].cantidad
+        // la compra con menor monto registrada en el mes de Agosto y el numero de producto comprado
+        if(compras[key][subkey].mes === MES_AGOSTO){
+            if(menorCompraAgosto === null || (menorCompraAgosto && montoCompra < menorCompraAgosto)){
+                menorCompraAgosto = montoCompra
+                nuProductoAgosto = compras[key][subkey].nuProducto
+            }
+            /* 
+            if(menorCompraAgosto === null){
+                menorCompraAgosto = montoCompra
+                nuProductoAgosto = compras[key][subkey].nuProducto
+            }else{
+                if(montoCompra < menorCompraAgosto){
+                    menorCompraAgosto = montoCompra
+                    nuProductoAgosto = compras[key][subkey].nuProducto
+                }
+            } 
+            */
+        }
+        // el numero de producto con mayor cantidad comprada y a que proveedor
+        if(compras[key][subkey].cantidad > maxCantidadComp){
+            maxCantidadComp = compras[key][subkey].cantidad
+            nuProductomaxCompra = compras[key][subkey].nuProducto
+            provedorMayorcantidadComprada = key
+        }
+
+
+
+        // total comprado por tipo de Factura
+        switch (compras[key][subkey].tipofactura) {
+            case 'A':{
+                tipoFacA += montoCompra
+                break;
+            }
+            case 'B':{
+                tipoFacB += montoCompra
+                break;
+            }
+            case 'C':{
+                tipoFacC += montoCompra
+                break;
+            }
+        }
+        //monto maximo registrado en una compra por cada proveddor y el numero de provedor
+        if(montoCompra > montoMaximo){
+            montoMaximo = montoCompra
+            kyeActual = key
+        }
     }
-    
-  /*   console.log(montoMaximo, kyeActual)
-    while(key === kyeActual){
-        //console.log(compras[key])
-    if(montoCompra > montoMaximo){
-        montoMaximo = montoCompra
-        console.log(montoMaximo, kyeActual)
-        continue
-    }
-    }
-     */
-}
+    console.log('el monto maximo es',montoMaximo,'del numero de proveedor', kyeActual);
+    // la cantidad de compras que se le realizo a cada proveedor
+    console.log('del proveedor', key, 'se realizaron', Object.keys(compras[key]).length, 'compras')
+}    
+
+console.log('tipo de factura A total',tipoFacA,'tipo de factura B total',tipoFacB,'tipo de factura C total',tipoFacC);
+console.log('la menor compra de Agosto fue de', menorCompraAgosto, 'del producto', nuProductoAgosto);
+console.log('el producto con mayor cantidad comprada es', nuProductomaxCompra, 'al numero de proveedor', provedorMayorcantidadComprada);
