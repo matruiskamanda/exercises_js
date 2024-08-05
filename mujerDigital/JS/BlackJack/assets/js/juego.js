edae259a724cec6a0aa86ce2de4b0b5280a1dd00
Amanda Torres <matriuskamanda@gmail.com>
2024-08-05 15:39:38 -0600

    funcion que se autoinvoca en BlackJack

--- mujerDigital/JS/BlackJack/assets/js/juego.js
+++ mujerDigital/JS/BlackJack/assets/js/juego.js
@@ -1,5 +1,7 @@
-
-let deck = [];
+// funcion anonima que se auto invoca por eso los parentesis extras
+(()=> {
+    'use strict'
+    let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 let puntosJugador = 0;
@@ -7,13 +9,15 @@ let puntosComputadora = 0;
 
 // referencias del html 
 const btnPedir = document.querySelector('#btnPedir');
+const btnNuevo = document.querySelector('#btnNuevo');
+const btnDetener = document.querySelector('#btnDetener');
+
 const idJugadorCartas = document.querySelector('#jugador-cartas');
 const idComputadoraCartas = document.querySelector('#computadora-cartas');
 const smalls = document.querySelectorAll('small');
-const btnDetener = document.querySelector('#btnDetener');
-const btnNuevo = document.querySelector('#btnNuevo');
 
-const creardeck = () => {
+// esta funcion crea un nuevo deck 
+const crearDeck = () => {
     for(let i =2; i <= 10; i++){
         for(let tipo of tipos){
             deck.push(i + tipo);
@@ -30,17 +34,18 @@ const creardeck = () => {
    return deck;
 }
 
-creardeck();
+crearDeck();
 
-
+// funcion para permitirme tomar carta 
 const pedirCarta = () => {
     if(deck.length === 0){
         throw ('No hay cartas en el deck')
     }
-    carta = deck.pop();
+    const carta = deck.pop();
     return carta;
 }
 
+// pedir carta
 const valorcarta = (carta) => {
     const valor = carta.substring(0,carta.length -1);
     return (!isNaN(valor)) ?
@@ -67,13 +72,13 @@ const turnoComputadora = (puntosMinimos) => {
 
     idComputadoraCartas.append(imgCarta);
 
-    if(puntosMinimos >= 21){
+    if(puntosMinimos > 21){
         break;
     }
         
     } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
     
-             puntosComputadora > puntosMinimos ? alert('Perdiste, suerte en la próxima ronda :( '): 
+            puntosComputadora > puntosMinimos ? alert('Perdiste, suerte en la próxima ronda :( '): 
             puntosComputadora < puntosMinimos ? alert('Ganaste, Genial! <3') : alert('Nadie Gana!')
 }
 
@@ -87,19 +92,22 @@ btnPedir.addEventListener('click', () => {
     const imgCarta = document.createElement('img');
     imgCarta.src = `./cartas/${carta}.png`;
     imgCarta.classList.add('carta');
-
     idJugadorCartas.append(imgCarta);
+
+    if(puntosJugador > 21){
+        console.warm("Lo siento perdiste")
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador)
+    }else if(puntosJugador === 21){
+        console.warm("21 Genial")
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora(puntosJugador)
+    }
 })
 
-if(puntosJugador > 21){
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    turnoComputadora(puntosJugador)
-}else if(puntosJugador === 21){
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    turnoComputadora(puntosJugador)
-}
+
 
 btnDetener.addEventListener('click', () => {
     btnDetener.disabled = true;
@@ -109,15 +117,23 @@ btnDetener.addEventListener('click', () => {
 })
 
 btnNuevo.addEventListener('click', () => {
-    deck = creardeck();
+    console.clear();
+    deck = [];
+    deck = crearDeck();
+
     puntosJugador = 0;
     puntosComputadora = 0;
-    smalls[0] = 0;
-    smalls[1] = 0;
+
+    smalls[0].innerText = 0;
+    smalls[1].innerText = 0;
 
     idComputadoraCartas.innerHTML = '';
     idJugadorCartas.innerHTML = '';
 
     btnDetener.disabled = false;
     btnPedir.disabled = false;
-})
+});
+})();
+
+
+
